76564fc42039dba32846dff13a47452444a7b55c
Gilson <gindrejunior@hotmail.com>
2022-02-03 07:37:37 -0300

    app views

--- views/login.py
+++ views/login.py
@@ -13,30 +13,30 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 
-class Ui_UI_Login(object):
-    def setupUi(self, UI_Login):
-        if not UI_Login.objectName():
-            UI_Login.setObjectName(u"UI_Login")
-        UI_Login.resize(607, 491)
-        UI_Login.setStyleSheet(u"background-color: rgb(122, 122, 122);")
-        self.verticalLayout = QVBoxLayout(UI_Login)
+class Ui_Login(object):
+    def setupUi(self, Login):
+        if not Login.objectName():
+            Login.setObjectName(u"Login")
+        Login.resize(607, 491)
+        Login.setStyleSheet(u"background-color: rgb(122, 122, 122);")
+        self.verticalLayout = QVBoxLayout(Login)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.top_frame = QFrame(UI_Login)
+        self.top_frame = QFrame(Login)
         self.top_frame.setObjectName(u"top_frame")
-        self.top_frame.setFrameShape(QFrame.StyledPanel)
+        self.top_frame.setFrameShape(QFrame.NoFrame)
         self.top_frame.setFrameShadow(QFrame.Raised)
 
         self.verticalLayout.addWidget(self.top_frame)
 
-        self.mid_frame = QFrame(UI_Login)
+        self.mid_frame = QFrame(Login)
         self.mid_frame.setObjectName(u"mid_frame")
-        self.mid_frame.setFrameShape(QFrame.StyledPanel)
+        self.mid_frame.setFrameShape(QFrame.NoFrame)
         self.mid_frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout = QHBoxLayout(self.mid_frame)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.mid_left_frame = QFrame(self.mid_frame)
         self.mid_left_frame.setObjectName(u"mid_left_frame")
-        self.mid_left_frame.setFrameShape(QFrame.StyledPanel)
+        self.mid_left_frame.setFrameShape(QFrame.NoFrame)
         self.mid_left_frame.setFrameShadow(QFrame.Raised)
 
         self.horizontalLayout.addWidget(self.mid_left_frame)
@@ -48,16 +48,17 @@ class Ui_UI_Login(object):
         self.mid_center_frame.setStyleSheet(u"QFrame {\n"
 "	background-color: rgb(57, 57, 57);\n"
 "}")
-        self.mid_center_frame.setFrameShape(QFrame.StyledPanel)
+        self.mid_center_frame.setFrameShape(QFrame.NoFrame)
         self.mid_center_frame.setFrameShadow(QFrame.Raised)
         self.verticalLayout_2 = QVBoxLayout(self.mid_center_frame)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.form_frame = QFrame(self.mid_center_frame)
         self.form_frame.setObjectName(u"form_frame")
-        self.form_frame.setFrameShape(QFrame.StyledPanel)
+        self.form_frame.setFrameShape(QFrame.NoFrame)
         self.form_frame.setFrameShadow(QFrame.Raised)
         self.verticalLayout_3 = QVBoxLayout(self.form_frame)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(-1, -1, -1, 0)
         self.username_label = QLabel(self.form_frame)
         self.username_label.setObjectName(u"username_label")
 
@@ -81,12 +82,30 @@ class Ui_UI_Login(object):
 
         self.verticalLayout_2.addWidget(self.form_frame)
 
+        self.info_frame = QFrame(self.mid_center_frame)
+        self.info_frame.setObjectName(u"info_frame")
+        self.info_frame.setMinimumSize(QSize(0, 10))
+        self.info_frame.setFrameShape(QFrame.NoFrame)
+        self.info_frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.info_frame)
+        self.horizontalLayout_3.setSpacing(0)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(-1, 0, -1, 0)
+        self.info_label = QLabel(self.info_frame)
+        self.info_label.setObjectName(u"info_label")
+
+        self.horizontalLayout_3.addWidget(self.info_label, 0, Qt.AlignHCenter)
+
+
+        self.verticalLayout_2.addWidget(self.info_frame)
+
         self.button_frame = QFrame(self.mid_center_frame)
         self.button_frame.setObjectName(u"button_frame")
-        self.button_frame.setFrameShape(QFrame.StyledPanel)
+        self.button_frame.setFrameShape(QFrame.NoFrame)
         self.button_frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_2 = QHBoxLayout(self.button_frame)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(-1, 0, -1, -1)
         self.login_button = QPushButton(self.button_frame)
         self.login_button.setObjectName(u"login_button")
         self.login_button.setMaximumSize(QSize(100, 16777215))
@@ -101,32 +120,35 @@ class Ui_UI_Login(object):
 
         self.mid_right_frame = QFrame(self.mid_frame)
         self.mid_right_frame.setObjectName(u"mid_right_frame")
-        self.mid_right_frame.setFrameShape(QFrame.StyledPanel)
+        self.mid_right_frame.setFrameShape(QFrame.NoFrame)
         self.mid_right_frame.setFrameShadow(QFrame.Raised)
+        self.mid_right_frame.setLineWidth(0)
 
         self.horizontalLayout.addWidget(self.mid_right_frame)
 
 
         self.verticalLayout.addWidget(self.mid_frame)
 
-        self.bot_frame = QFrame(UI_Login)
+        self.bot_frame = QFrame(Login)
         self.bot_frame.setObjectName(u"bot_frame")
-        self.bot_frame.setFrameShape(QFrame.StyledPanel)
+        self.bot_frame.setFrameShape(QFrame.NoFrame)
         self.bot_frame.setFrameShadow(QFrame.Raised)
+        self.bot_frame.setLineWidth(0)
 
         self.verticalLayout.addWidget(self.bot_frame)
 
 
-        self.retranslateUi(UI_Login)
+        self.retranslateUi(Login)
 
-        QMetaObject.connectSlotsByName(UI_Login)
+        QMetaObject.connectSlotsByName(Login)
     # setupUi
 
-    def retranslateUi(self, UI_Login):
-        UI_Login.setWindowTitle(QCoreApplication.translate("UI_Login", u"Savefor - Login", None))
-        self.username_label.setText(QCoreApplication.translate("UI_Login", u"Username", None))
+    def retranslateUi(self, Login):
+        Login.setWindowTitle(QCoreApplication.translate("Login", u"Savefor - Login", None))
+        self.username_label.setText(QCoreApplication.translate("Login", u"Username", None))
         self.username_lineEdit.setText("")
-        self.password_label.setText(QCoreApplication.translate("UI_Login", u"Password", None))
-        self.login_button.setText(QCoreApplication.translate("UI_Login", u"Login", None))
+        self.password_label.setText(QCoreApplication.translate("Login", u"Password", None))
+        self.info_label.setText("")
+        self.login_button.setText(QCoreApplication.translate("Login", u"Login", None))
     # retranslateUi
 
--- views/upload.py
+++ views/upload.py
@@ -13,25 +13,26 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 
-class Ui_UI_Upload(object):
-    def setupUi(self, UI_Upload):
-        if not UI_Upload.objectName():
-            UI_Upload.setObjectName(u"UI_Upload")
-        UI_Upload.resize(728, 527)
-        self.verticalLayout = QVBoxLayout(UI_Upload)
+class Ui_Upload(object):
+    def setupUi(self, Upload):
+        if not Upload.objectName():
+            Upload.setObjectName(u"Upload")
+        Upload.resize(728, 527)
+        Upload.setStyleSheet(u"background-color: rgb(57, 57, 57);")
+        self.verticalLayout = QVBoxLayout(Upload)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.buttons_frame = QFrame(UI_Upload)
+        self.buttons_frame = QFrame(Upload)
         self.buttons_frame.setObjectName(u"buttons_frame")
         self.buttons_frame.setMinimumSize(QSize(0, 80))
         self.buttons_frame.setMaximumSize(QSize(16777215, 80))
-        self.buttons_frame.setFrameShape(QFrame.StyledPanel)
+        self.buttons_frame.setFrameShape(QFrame.NoFrame)
         self.buttons_frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout = QHBoxLayout(self.buttons_frame)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.select_frame = QFrame(self.buttons_frame)
         self.select_frame.setObjectName(u"select_frame")
         self.select_frame.setMaximumSize(QSize(100, 16777215))
-        self.select_frame.setFrameShape(QFrame.StyledPanel)
+        self.select_frame.setFrameShape(QFrame.NoFrame)
         self.select_frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_3 = QHBoxLayout(self.select_frame)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
@@ -45,7 +46,7 @@ class Ui_UI_Upload(object):
 
         self.filename_frame = QFrame(self.buttons_frame)
         self.filename_frame.setObjectName(u"filename_frame")
-        self.filename_frame.setFrameShape(QFrame.StyledPanel)
+        self.filename_frame.setFrameShape(QFrame.NoFrame)
         self.filename_frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_4 = QHBoxLayout(self.filename_frame)
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
@@ -60,7 +61,7 @@ class Ui_UI_Upload(object):
         self.upload_frame = QFrame(self.buttons_frame)
         self.upload_frame.setObjectName(u"upload_frame")
         self.upload_frame.setMaximumSize(QSize(100, 16777215))
-        self.upload_frame.setFrameShape(QFrame.StyledPanel)
+        self.upload_frame.setFrameShape(QFrame.NoFrame)
         self.upload_frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_2 = QHBoxLayout(self.upload_frame)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
@@ -72,12 +73,28 @@ class Ui_UI_Upload(object):
 
         self.horizontalLayout.addWidget(self.upload_frame)
 
+        self.logout_frame = QFrame(self.buttons_frame)
+        self.logout_frame.setObjectName(u"logout_frame")
+        self.logout_frame.setMinimumSize(QSize(80, 0))
+        self.logout_frame.setMaximumSize(QSize(80, 16777215))
+        self.logout_frame.setFrameShape(QFrame.NoFrame)
+        self.logout_frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_11 = QHBoxLayout(self.logout_frame)
+        self.horizontalLayout_11.setObjectName(u"horizontalLayout_11")
+        self.logout_button = QPushButton(self.logout_frame)
+        self.logout_button.setObjectName(u"logout_button")
+
+        self.horizontalLayout_11.addWidget(self.logout_button)
+
+
+        self.horizontalLayout.addWidget(self.logout_frame)
+
 
         self.verticalLayout.addWidget(self.buttons_frame)
 
-        self.content_frame = QFrame(UI_Upload)
+        self.content_frame = QFrame(Upload)
         self.content_frame.setObjectName(u"content_frame")
-        self.content_frame.setFrameShape(QFrame.StyledPanel)
+        self.content_frame.setFrameShape(QFrame.NoFrame)
         self.content_frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_5 = QHBoxLayout(self.content_frame)
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
@@ -94,83 +111,9 @@ class Ui_UI_Upload(object):
         self.upload_scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 708, 424))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 710, 426))
         self.verticalLayout_2 = QVBoxLayout(self.scrollAreaWidgetContents)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.file_frame = QFrame(self.scrollAreaWidgetContents)
-        self.file_frame.setObjectName(u"file_frame")
-        self.file_frame.setMinimumSize(QSize(0, 80))
-        self.file_frame.setMaximumSize(QSize(16777215, 80))
-        self.file_frame.setFrameShape(QFrame.StyledPanel)
-        self.file_frame.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_6 = QHBoxLayout(self.file_frame)
-        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
-        self.frame = QFrame(self.file_frame)
-        self.frame.setObjectName(u"frame")
-        self.frame.setMinimumSize(QSize(150, 0))
-        self.frame.setMaximumSize(QSize(150, 16777215))
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_7 = QHBoxLayout(self.frame)
-        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
-        self.sending_filename_label = QLabel(self.frame)
-        self.sending_filename_label.setObjectName(u"sending_filename_label")
-
-        self.horizontalLayout_7.addWidget(self.sending_filename_label)
-
-
-        self.horizontalLayout_6.addWidget(self.frame)
-
-        self.frame_2 = QFrame(self.file_frame)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setMinimumSize(QSize(150, 0))
-        self.frame_2.setMaximumSize(QSize(150, 16777215))
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_8 = QHBoxLayout(self.frame_2)
-        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
-        self.time_label = QLabel(self.frame_2)
-        self.time_label.setObjectName(u"time_label")
-
-        self.horizontalLayout_8.addWidget(self.time_label)
-
-
-        self.horizontalLayout_6.addWidget(self.frame_2)
-
-        self.frame_3 = QFrame(self.file_frame)
-        self.frame_3.setObjectName(u"frame_3")
-        self.frame_3.setFrameShape(QFrame.StyledPanel)
-        self.frame_3.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_10 = QHBoxLayout(self.frame_3)
-        self.horizontalLayout_10.setObjectName(u"horizontalLayout_10")
-        self.progressBar = QProgressBar(self.frame_3)
-        self.progressBar.setObjectName(u"progressBar")
-        self.progressBar.setValue(24)
-
-        self.horizontalLayout_10.addWidget(self.progressBar)
-
-
-        self.horizontalLayout_6.addWidget(self.frame_3)
-
-        self.frame_4 = QFrame(self.file_frame)
-        self.frame_4.setObjectName(u"frame_4")
-        self.frame_4.setMinimumSize(QSize(80, 0))
-        self.frame_4.setMaximumSize(QSize(80, 16777215))
-        self.frame_4.setFrameShape(QFrame.StyledPanel)
-        self.frame_4.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_9 = QHBoxLayout(self.frame_4)
-        self.horizontalLayout_9.setObjectName(u"horizontalLayout_9")
-        self.cancel_button = QPushButton(self.frame_4)
-        self.cancel_button.setObjectName(u"cancel_button")
-
-        self.horizontalLayout_9.addWidget(self.cancel_button)
-
-
-        self.horizontalLayout_6.addWidget(self.frame_4)
-
-
-        self.verticalLayout_2.addWidget(self.file_frame)
-
         self.upload_scrollArea.setWidget(self.scrollAreaWidgetContents)
 
         self.horizontalLayout_5.addWidget(self.upload_scrollArea)
@@ -179,18 +122,16 @@ class Ui_UI_Upload(object):
         self.verticalLayout.addWidget(self.content_frame)
 
 
-        self.retranslateUi(UI_Upload)
+        self.retranslateUi(Upload)
 
-        QMetaObject.connectSlotsByName(UI_Upload)
+        QMetaObject.connectSlotsByName(Upload)
     # setupUi
 
-    def retranslateUi(self, UI_Upload):
-        UI_Upload.setWindowTitle(QCoreApplication.translate("UI_Upload", u"Savefor - Upload", None))
-        self.select_button.setText(QCoreApplication.translate("UI_Upload", u"Select", None))
-        self.filename_label.setText(QCoreApplication.translate("UI_Upload", u"test_file.txt", None))
-        self.upload_button.setText(QCoreApplication.translate("UI_Upload", u"Upload", None))
-        self.sending_filename_label.setText(QCoreApplication.translate("UI_Upload", u"test_file.txt", None))
-        self.time_label.setText(QCoreApplication.translate("UI_Upload", u"TextLabel", None))
-        self.cancel_button.setText(QCoreApplication.translate("UI_Upload", u"PushButton", None))
+    def retranslateUi(self, Upload):
+        Upload.setWindowTitle(QCoreApplication.translate("Upload", u"Savefor - Upload", None))
+        self.select_button.setText(QCoreApplication.translate("Upload", u"Select", None))
+        self.filename_label.setText("")
+        self.upload_button.setText(QCoreApplication.translate("Upload", u"Upload", None))
+        self.logout_button.setText(QCoreApplication.translate("Upload", u"Logout", None))
     # retranslateUi
 
